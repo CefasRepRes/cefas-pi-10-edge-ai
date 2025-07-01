8c2a4deedfc3e9557ab1fa338eab255cc9241b2f
Joe <42866685+JoeRibeiro@users.noreply.github.com>
2025-07-01 13:54:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,5 @@ protocol_version = "0.0.3"
 # connstr = os.environ["SERVICE_BUS_CONNECTION_STR"]
 # queue_name = os.environ["SERVICE_BUS_QUEUE_NAME"]
 
-# connstr = "Endpoint=sb://rb26test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DQJ32qm8N+AHe6zrR1afVqIsKo+Fn9+Ii+ASbLSQaCc="
-# queue_name = "test"
-
-connstr = "Endpoint=sb://cit-rd-plankton-svcbus.servicebus.windows.net/;SharedAccessKeyName=RVSend;SharedAccessKey=Debq0m294NwrRu+SyDBeD/REAvzC7JVBD+ASbMTxons="
+connstr = "Endpoint=sb://cit-rd-plankton-svcbus.servicebus.windows.net/;SharedAccessKeyName=RVSend;SharedAccessKey=something_like_Debq0m294NwrRu+SyDBeD/REAvzC7JVBD+ASbMTxons="
 queue_name = "rv-dashboard"
